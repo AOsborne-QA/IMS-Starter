2d55e793f3c441b3bb0e68dfa0ac594a6baa4a08
AOsborne-QA <AOsborne@academytrainee.com>
2020-11-11 15:43:53 +0000

    Add functionality to read items in database

--- src/main/java/com/qa/ims/controller/ItemsController.java
+++ src/main/java/com/qa/ims/controller/ItemsController.java
@@ -5,16 +5,36 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.qa.ims.persistence.dao.ItemsDAO;
 import com.qa.ims.persistence.domain.Items;
+import com.qa.ims.utils.Utils;
+
+
+// Takes in item details to enable CRUD functionality
 
 public class ItemsController implements CrudController<Items> {
 	
 	public static final Logger LOGGER = LogManager.getLogger();
+	
+	private ItemsDAO itemsDAO;
+	private Utils utils;
+	
+	public ItemsController(ItemsDAO itemsDAO, Utils utils) {
+		super();
+		this.itemsDAO = itemsDAO;
+		this.utils = utils;
+	}
+	
+	
+	// Reads all items to the logger
 
 	@Override
 	public List<Items> readAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Items> items = itemsDAO.readAll();
+		for (Items item : items) {
+			LOGGER.info(item.toString());
+		}
+		return items;
 	}
 
 	@Override
