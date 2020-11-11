ad0a9f196dfe05c709c91f0cb8af8d73d9c099ea
AOsborne-QA <AOsborne@academytrainee.com>
2020-11-11 15:35:56 +0000

    Import logger and implement crudcontroller

--- src/main/java/com/qa/ims/controller/ItemsController.java
+++ src/main/java/com/qa/ims/controller/ItemsController.java
@@ -1,5 +1,38 @@
 package com.qa.ims.controller;
 
-public class ItemsController {
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import com.qa.ims.persistence.domain.Items;
+
+public class ItemsController implements CrudController<Items> {
+	
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	@Override
+	public List<Items> readAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Items create() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Items update() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int delete() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
