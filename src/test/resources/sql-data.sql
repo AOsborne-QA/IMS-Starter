121c2595dc104e96bf7e20387019924a4dc517f1
AOsborne-QA <AOsborne@academytrainee.com>
2020-11-17 14:52:48 +0000

    Add test data for customers and items

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,1 +1,4 @@
 INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('Shadow', 'Cat');
+INSERT INTO `ims`.`items` (`item_name`, `item_price`) VALUES ('Mouse', 1.99);
+INSERT INTO `ims`.`items` (`item_name`, `item_price`) VALUES ('Treats', 6.99);
